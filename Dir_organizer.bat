e0c30862f955bfe47b35143899693ef56aef0feb
Islam Gomaa <147432821+iislamgom3a@users.noreply.github.com>
2025-03-19 00:53:20 +0200

    Update Dir_organizer.bat

--- Dir_organizer.bat
+++ Dir_organizer.bat
@@ -12,13 +12,11 @@ for %%d in (Images Videos Software Archives Docs Audio src spreadsheets presenta
 :: Function to move files based on extension
 call :move_files "Images" jpg jpeg png gif bmp tiff webp heic raw svg JPG
 call :move_files "Videos" mp4 mkv mov avi flv webm mpeg 3gp ts wav
-call :move_files "Software" exe msi deb rpm dmg pkg appimage sh ps1 jar bin run
-call :move_files "Docs" doc docx dot dotx xls xlsx xlsm xlt xltx ppt pptx pps ppsx pot potx mdb accdb pub one odt ods odp odg odf pdf xps txt rtf csv log md xml html htm json tex epub chm djvu
-call :move_files "Archives" zip rar 7z tar gz bz2 xz iso cab arj lzh ace z arc jar war apk deb rpm pkg tar.gz tar.bz2 tar.xz tgz tbz txz
+call :move_files "Software" exe msi deb rpm dmg  appimage sh jar bin run
+call :move_files "Docs" doc docx dot dotx xls xlsx xlsm xlt xltx ppt pptx pps ppsx pot potx mdb accdb pub one odt ods odp odg odf pdf xps txt rtf csv  tex epub chm djvu
+call :move_files "Archives" zip rar 7z tar gz bz2 xz iso cab arj lzh ace z arc jar war apk deb pkg tar.gz tar.bz2 tar.xz tgz tbz txz
 call :move_files "Audio" mp3 m4a wav flac aac ogg opus wma alac amr aiff au midi mid
-call :move_files "src" py cpp c java ipynb js ts cs rb go rs swift kt php html css json yaml yml xml sh ps1 r lua dart scala pl pm ml hs jl lisp clj ex exs erl hrl pas vhdl verilog asm sql awk tex md toml ini cfg makefile dockerfile jenkinsfile gitignore env
-call :move_files "spreadsheets" xls xlsx csv ods numbers 
-call :move_files "presentations" ppt pptx pps ppsx key odp
+call :move_files "src" py cpp c java ipynb js ts cs rb go rs swift kt php html css json yaml yml xml sh ps1 r lua dart scala pl pm ml hs jl lisp clj ex exs erl hrl pas vhdl verilog asm sql awk tex  toml ini cfg makefile dockerfile jenkinsfile gitignore env log 
 call :move_files "fonts" ttf otf woff woff2 eot
 
 
